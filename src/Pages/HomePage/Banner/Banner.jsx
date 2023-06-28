accdffaa910c0790addf50c9b9842a762de2e14a
Doha0 <abu.jardoha@gmail.com>
2023-06-28 17:35:29 +0600

    bannar style updated

--- src/Pages/HomePage/Banner/Banner.jsx
+++ src/Pages/HomePage/Banner/Banner.jsx
@@ -3,13 +3,15 @@ import background from '../../../assets/mesh.png'
 
 const Banner = () => {
     return (
-        <div className='space-y-2 lg:space-y-4 py-6 px-4 lg:py-24 lg:px-20 bg-cover bg-center bg-no-repeat object-cover' style={{ backgroundImage: `url(${background})` }}>
-            <p className='font-medium uppercase text-xs lg:text-xl'>Official Refurbished</p>
-            <div className='font-semibold text-base lg:text-4xl space-y-1 lg:space-y-2'>
-                <h2 >Save 20%</h2>
-                <h2 >Certified Quality, Full Warranty</h2>
+        <div className='hero min-h-screen'
+            style={{ backgroundImage: `url(${background})` }}>
+            <div className="hero-content text-center ">
+                <div className="max-w-md">
+                    <p className="mb-5">Hello there</p>
+                    <h1 className="mb-5 text-5xl font-bold">I am Abu Jar Gifary Doha</h1>
+                    <button className="btn btn-ghost">Get Started</button>
+                </div>
             </div>
-            <button className="btn bg-slate-950 text-white ">Shop Now</button>
         </div>
     );
 };
